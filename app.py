5ca7618a85d5046d94898d352f82af753f59efb4
tobias <buecktobias@gmail.com>
2020-10-11 12:12:35 +0200

    created portfolio template and redirected to it

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
-
+from flask import render_template, redirect, url_for
 app = Flask(__name__)
+
+
+@app.route('/de/portfolio/')
+def portfolio_page():
+    return render_template("german_portfolio.html")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect(url_for("portfolio_page"))
 
 
 if __name__ == '__main__':
