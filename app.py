6176cd7e1669300a493ce0c9fba0c1131268d484
tobias <buecktobias@gmail.com>
2020-10-18 20:48:21 +0200

    drawing

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask import render_template, redirect, url_for
 from flask import request
 import json
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def get_languages():
 
 
 def get_default_language():
-    return "de"
+    return "en"
 
 
 def get_texts(language_code):
@@ -19,12 +20,14 @@ def get_texts(language_code):
         language_texts = json.load(f)
     return language_texts.get(language_code)
 
+
 @app.route("/sitemap/")
 def sitemap():
     return redirect(url_for('static', filename="sitemap.xml"))
 
+
 @app.route('/<language_code>/portfolio/')
-def portfolio_page(language_code):
+def portfolio_page(language_code="de"):
     if language_code not in get_languages():
         return redirect(url_for("portfolio_page", language_code=get_default_language()))
     texts = get_texts(language_code)
@@ -34,7 +37,14 @@ def portfolio_page(language_code):
 
 
 @app.route("/<language_code>/blogs/nlp-with-trip-advisor-reviews")
-def blog_nlp(language_code):
+def blogs(language_code="de"):
+    if request.cookies["languageCode"] != language_code:
+        return redirect(url_for("blogs", language_code=request.cookies["languageCode"]))
+    return "lol"
+
+
+@app.route("/<language_code>/blogs/nlp-with-trip-advisor-reviews")
+def blog_nlp(language_code="de"):
     if language_code != "en":
         return redirect(url_for("blog_nlp", language_code="en"))
     else:
@@ -43,13 +53,35 @@ def blog_nlp(language_code):
         return render_template("nlp_blog.html", **all_)
 
 
-@app.route('/')
-def hello_world():
+@app.route("/draw/")
+def draw_game():
+    return render_template("draw.html")
+
+
+@app.route("/draw/save", methods=["POST"])
+def draw_game_save():
+    data = request.form["data"]
+    filename = request.form["filename"]
+    with open(f"static/drawings/{filename}.txt", "w") as f:
+        f.write(data)
+    return render_template("draw.html")
+
+@app.route('/get_language/<redirect_url>/')
+def get_language(redirect_url):
+    print(url_for("home"))
     try:
         best_language = request.accept_languages.best_match(get_languages())
     except:
         best_language = "de"
-    return redirect(url_for("portfolio_page", language_code=best_language))
+    resp = make_response(redirect(redirect_url.replace("??????", "/")))
+    resp.set_cookie('languageCode', best_language)
+    return resp
+
+@app.route('/')
+def home():
+    if "languageCode" in request.cookies:
+        return redirect(url_for("portfolio_page", language_code=request.cookies["languageCode"]))
+    return redirect(url_for("get_language", redirect_url=urllib.parse.quote(request.url).replace("/", "??????")))
 
 
 if __name__ == '__main__':
